890178e4b482e9d5312ccbb34d9b5444f4f99f7c
忠庭 蔡 <tom@m.maxpower.com.tw>
2021-04-28 09:43:27 +0800

    @增加 共用基本類別減少 class的建立 --新增 AllMethodsHandler :共用基本類別 --新增 DataHandler :實做AllMethodsHandler

---
+++ StartEventBusTest/Handler/AllMethodsHandler.cs
@@ -0,0 +1,39 @@
+﻿using EventBusCore.Events;
+using EventBusCore.Handler;
+using StartEventBusTest.EventBusSet;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartEventBusTest.Handler
+{
+    public abstract class AllMethodsHandler<T>  where T : IEventBase
+    {
+
+        public abstract Task EventStart(T @event);
+        public abstract Task EventStop(T @event);
+
+        public class EventKeyStopHandler : IEventHandler<T>
+        {
+            AllMethodsHandler<T> _AllHandler;
+            public EventKeyStopHandler(AllMethodsHandler<T> allHandler)
+            {
+                this._AllHandler = allHandler;
+            }
+            public Task Handle(T @event) => _AllHandler.EventStop(@event);
+        }
+        public class EventKeyStartHanlder : IEventHandler<T>
+        {
+            AllMethodsHandler<T> _AllHandler;
+            public EventKeyStartHanlder(AllMethodsHandler<T> allHandler)
+            {
+                this._AllHandler = allHandler;
+            }
+            public Task Handle(T @event) => _AllHandler.EventStart(@event);
+        }
+
+
+    }
+
+}
---
+++ StartEventBusTest/Handler/DataHandler.cs
@@ -0,0 +1,23 @@
+﻿using StartEventBusTest.EventBusSet;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartEventBusTest.Handler
+{
+    public class DataHandler : AllMethodsHandler<ShowEvent>  
+    {
+        public override Task EventStart(ShowEvent @event)
+        {
+            Console.WriteLine($"{@event.Id}-{@event.DateTime}-DataHandler");
+            return Task.FromResult(true);
+        }
+
+        public override Task EventStop(ShowEvent @event)
+        {
+            Console.WriteLine($"{@event.Id}-{@event.DateTime}-DataHandler");
+            return Task.FromResult(true);
+        }
+    }
+}
